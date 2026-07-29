0839057e16d431ad9b2183ef39606094ff80979e
Շուշանիկ Աraakelyan <shushanikarakl62@gmail.com>
2026-07-29 21:56:34 +0400

    bug fix

--- backend/src/services/authService.js
+++ backend/src/services/authService.js
@@ -2,25 +2,25 @@ import User from "../models/Users";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken"
 
-export const registerUserService = async (name, email, password)=>{
-   try{
-     const userExicts = await User.findOne({email})
-     if(userExicts){
-        throw new Error("User with this email already exists")
-     }else{
-      const hashedPassword =  await bcrypt.hash(password, 10); 
-   const newUser =  await   User.create({
-name: name,
-email: email, 
-password: hashedPassword
-     })
-     return newUser
-     }
-     
-   }catch(err){
-    throw new Error(err.message)
-   }
-    
+export const registerUserService = async (name, email, password) => {
+    try {
+        const userExicts = await User.findOne({ email })
+        if (userExicts) {
+            throw new Error("User with this email already exists")
+        } else {
+            const hashedPassword = await bcrypt.hash(password, 10);
+            const newUser = await User.create({
+                name: name,
+                email: email,
+                password: hashedPassword
+            })
+            return newUser
+        }
+
+    } catch (err) {
+        throw new Error(err.message)
+    }
+
 };
 
 const payload = {
@@ -28,16 +28,16 @@ const payload = {
     role: user.role
 }
 
-export const loginUserService = async(email,password)=>{
-    try{
-    const user = User.findOne({email})
-    if(!user)throw new Error("Invalid credentials")
-        const pass = bcrypt.compare(password, user.password)
-    if(!pass) throw new Error("Invalid credentials");
-    const secret = process.env.SECRET
-    const token = jwt.sign(payload, secret)
-    return user
-    }catch(err){
+export const loginUserService = async (email, password) => {
+    try {
+        const user = await User.findOne({ email })
+        if (!user) throw new Error("Invalid credentials")
+        const pass = await bcrypt.compare(password, user.password)
+        if (!pass) throw new Error("Invalid credentials");
+        const secret = process.env.SECRET
+        const token = jwt.sign(payload, secret)
+        return { user, token }
+    } catch (err) {
         throw new Error(err.message)
     }
 
