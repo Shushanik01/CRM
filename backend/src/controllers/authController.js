7bc30d269f8fb2d1b080b1d2f50bdea921eb9ded
Շուշանիկ Աraakelyan <shushanikarakl62@gmail.com>
2026-07-31 21:59:20 +0400

    checking ownership middleware

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -31,5 +31,22 @@ export const getUserInfo = (req, res) => {
         res.status(200).json({ user: req.user })
     } catch (err) {
         res.status(400).json({ message: err.message })
-    }
+    };
 }
+
+export const checkOwnership = (Model, paramName = 'id') => async (req, res, next) => {
+    try {
+        const resourse = await Model.findById(req.params[paramName]);
+
+        if (!resourse) {
+            return res.status(404).json({ message: 'Not found' })
+        };
+
+        if (req.user.role === 'admin' || resourse.createdBy.toString() === req.user.id) {
+            return next()
+        }
+        return res.status(403).json({ message: 'Not authorized to modify resource' })
+    } catch (err) {
+        return res.status(400).json(err.message)
+    }
+};
