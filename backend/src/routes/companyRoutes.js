c44a82795f3935c0dfeb82114ed4869c5fb67aeb
Շուշանիկ <shushanikarakl62@gmail.com>
2026-08-01 07:41:57 +0000

    main.tsx setup

--- backend/src/routes/companyRoutes.js
+++ backend/src/routes/companyRoutes.js
@@ -3,7 +3,7 @@ import { addCompany, getALlCompany} from "../controllers/companyControllers";
 
 const companyRoutes = express.Router();
 companyRoutes.post("/company", addCompany);
-companyRoutes.get("/company", getALlCompany)
+companyRoutes.get("/company", getALlCompany )
 
 
 export default companyRoutes
