288a62ee7aadd823c25d05cae6a016491537b814
Շուշանիկ Աraakelyan <shushanikarakl62@gmail.com>
2026-08-01 20:29:13 +0400

    Login page & bug fixes & study notes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,22 +1,48 @@
-import {useForm} from '@tanstack/react-query'
+import { useForm } from 'react-hook-form';
+import { useMutation } from '@tanstack/react-query';
+import { loginUser } from '../api/authApi';
+import { useDispatch } from '@reduxjs/toolkit';
+import { useNavigate } from 'react-router-dom';
+import { setCredentials } from '../features/auth/authSlice.js'
 
-export const Login = () => { 
+export const Login = () => {
 
-    const { register, handleSubmit, formState : {errors}} = useForm()
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const mutation = useMutation({
+        mutationFn: loginUser,
+        onSuccess: (data) => {
+            dispatch(setCredentials(data))
+            navigate('dashboard')
+        }
+    });
+
+    const onSubmit = (formData) => {
+        mutation.mutate(formData)
+    };
 
     return (
         <form action="submit">
 
             <label htmlFor="email"></label>
             <input type="text" placeholder="email" name="email"
-            {...register('email', {required: true, pattern: /^\S+@\S+$/i})}
-            />    
+                {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
+            />
+            {errors.email && <p>{errors.email.message}</p>}
 
             <label htmlFor="password"></label>
-            <input type="text" placeholder="password" name="password"/>
+            <input type="text" placeholder="password" name="password"
+                {...register('password', { required: true })}
+            />
+            {errors.password && <p>{errors.password.message}</p>}
 
-            <button></button>
+            <button>{mutation.isPending ? 'Logging In...' : 'LogIn'} </button>
+            {mutation.isError && (
+                <p>{mutation.error.response?.data?.message || 'Login failed'}</p>
+            )}
         </form>
     )
 }
